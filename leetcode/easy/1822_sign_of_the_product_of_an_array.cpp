899901f08a64b5be56ccb6d7278dcbc71bed3245
Pierre Zins <zins.pierre@gmail.com>
2021-10-08 16:46:40 +0200

    Update

---
+++ leetcode/easy/1822_sign_of_the_product_of_an_array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int neg = 0;
+        for(auto n : nums)
+        {
+            if(n == 0) return 0;
+            if(n < 0) neg++;
+        }
+        if(neg % 2) return -1;
+        return 1;
+    }
+};
